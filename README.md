47819aafc57a21aa894d764a317cd270aa922092
Paspuleti <paspul@cooper.edu>
2018-05-08 01:03:13 -0400

    Final Project Submission

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # finalproject
+
+To use the program follow these steps:
+Type the name of the jpg you would like to test into a text file with the designated name
+run ./executingPyAndCppFiles
+Output appears on the terminal
+
+Install:
+
+numpy
+OpenCV2
+IndicoIo
+matplotlib
+pandas
--- data.cpp
+++ data.cpp
@@ -5,6 +5,9 @@
 #include <string>
 using namespace std;
 
+/*Defining class Person with members and
+ * member functions
+ */
 class Person{
         public:
 		vector<string> emotions;
@@ -16,26 +19,35 @@ class Person{
 		void displayMax();
 		void temp();
 };
-
+/* defining function getValues
+ * getValues populates the vector of doubles that
+ * is a member of Person
+ */
 void Person::getValues(){
 	for(auto in = emotions.begin(); in != emotions.end(); ++in){
 		in++;
 		emotionval.push_back(stod(*in));
 	}	
 }
-
+/* defining fuction display
+ * display lists the emotion along with its value
+ */
 void Person::display(){
 	for(auto in = emotions.begin(); in != emotions.end(); ++in){
 		cout<<*in<<endl;
 	}
 }
-
+/*defining fuction temp
+ * displays only the double values of each emotion in order
+ */
 void Person::temp(){
 	for(auto in = emotionval.begin(); in != emotionval.end(); ++in){
 		cout<<*in<<endl;
 	}
 }
-
+/* defining function displayMax
+ * displays the emotion with the highest value
+ */
 void Person::displayMax(){
 	double compare = emotionval[0];
 	int index = 0;
@@ -48,20 +60,22 @@ void Person::displayMax(){
 	cout<<"Highest emotion: "<< emotions[(index*2)]<<compare<<endl;
 }
 
+
 int main(){
 	cout<<"C++ File executing"<<endl;
 	vector<Person *> people;
 	string stuff;
 	string words;
 	vector<string> hold;
-
+	//opens the textfile Test to read data gathered from the jpgs
 	ifstream input ("Test");
 	if(input.is_open()){
 		while(getline(input,stuff)){
 			stringstream read(stuff);
+			//pushes strings into the vector of strings
 			while(read >> words)
 				hold.push_back(words);
-			Person *insert = new Person(hold);
+			Person *insert = new Person(hold);//creates a new person for each input
 			hold.clear();
 			people.push_back(insert);
 
@@ -72,8 +86,8 @@ int main(){
 	else
 		cout<<"Can't open file"<<endl;
 
-        for(auto in = people.begin(); in != people.end(); ++in){
-		//(*in)->display();
+        //Displays data by iteration through each Person object
+	for(auto in = people.begin(); in != people.end(); ++in){
 		(*in)->getValues();
 		(*in)->temp();
 		(*in)->displayMax();
